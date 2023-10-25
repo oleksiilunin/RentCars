2c87e99c948edd067616ad8c691f0bc0280d06b8
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-10-25 05:57:39 +0300

    Add redux and persist provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import { persistor, store } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './index.css';
+import App from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter
+        // basename="/RentCars"
+        >
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
