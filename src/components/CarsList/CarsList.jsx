9573674bb0ba6740c639266d2d4e66a506725a2d
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-10-26 23:09:03 +0300

    Add rules for filter

--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -3,13 +3,48 @@ import { List } from './CarsList.styled';
 import CarsItem from 'components/CarsItem';
 import PopUp from 'components/PopUp';
 import ButtonLoadMore from 'components/ButtonLoadMore/ButtonLoadMore';
+import { useSelector } from 'react-redux';
+import { selectFilter } from 'redux/filter/filterSelectors';
 
 export default function CarsList({ carsArray }) {
   const [isPopUpOpen, setIsPopUpOpen] = useState(false);
   const [selectedCar, setSelectedCar] = useState(null);
   const [displayedCars, setDisplayedCars] = useState(12);
 
-  const initialCars = carsArray.slice(0, displayedCars);
+  const filter = useSelector(selectFilter);
+
+  const filterCars = (cars, filter) => {
+    return cars.filter(car => {
+      const numericPrice = parseFloat(car.rentalPrice.slice(1));
+
+      // Фільтр за брендом
+      if (
+        filter.carBrand &&
+        car.make.toLowerCase().trim() !== filter.carBrand.toLowerCase().trim()
+      ) {
+        return false;
+      }
+
+      // Фільтр за ціною (за замовчуванням priceTo = 999999)
+      if (filter.pricePerOneHour && numericPrice > filter.pricePerOneHour) {
+        return false;
+      }
+
+      // Фільтр за пробігом (за замовчуванням MileageFrom = 0, MileageTo = 999999)
+      if (
+        (filter.carMileageFrom && car.mileage < filter.carMileageFrom) ||
+        (filter.carMileageTo && car.mileage > filter.carMileageTo)
+      ) {
+        return false;
+      }
+      return true; // Повертає true, якщо автомобіль відповідає всім умовам фільтра.
+    });
+  };
+
+  // Фільтруйте список машин на основі фільтрів
+  const filteredCars = filterCars(carsArray, filter);
+
+  const initialCars = filteredCars.slice(0, displayedCars);
 
   const openPopUp = () => {
     setIsPopUpOpen(true);
@@ -30,7 +65,7 @@ export default function CarsList({ carsArray }) {
   };
 
   return (
-    !!carsArray.length && (
+    !!filteredCars.length && (
       <>
         <List>
           {initialCars.map(car => (
@@ -43,7 +78,7 @@ export default function CarsList({ carsArray }) {
             </li>
           ))}
         </List>
-        {displayedCars < carsArray.length && (
+        {displayedCars < filteredCars.length && (
           <ButtonLoadMore onClick={handleLoadMore} />
         )}
         {isPopUpOpen && (
