4c5b4c8ddb29ce38b9234f0bf502d7af56b63437
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-10-26 23:11:16 +0300

    Add basename to route for github

--- src/index.js
+++ src/index.js
@@ -14,9 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter
-        // basename="/RentCars"
-        >
+        <BrowserRouter basename="/RentCars">
           <App />
         </BrowserRouter>
       </PersistGate>
