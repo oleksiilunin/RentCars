980c94698d6bdd8d0430396c399a30473112e6f7
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-10-29 11:35:26 +0200

    Refactoring code for Page404

--- src/pages/Page404/Page404.jsx
+++ src/pages/Page404/Page404.jsx
@@ -1,10 +1,13 @@
 import { scrollToTop } from 'assets/helpers/scrollToTop'
-import Button from 'components/Button/Button'
+// import Button from 'components/Button/Button'
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 // import image404 from '../../assets/images/404_BLUE.jpg'
-import { ImageStyled, LinkStyled, PageContainer } from './Page404.styled'
+import { 
+	// ImageStyled
+	// ,
+	 LinkStyled, PageContainer } from './Page404.styled'
 // import SectionWrapper from 'components/SectionWrapper'
 
 export default function Page404() {
